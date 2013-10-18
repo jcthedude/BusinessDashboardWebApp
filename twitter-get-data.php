a48d3f6c05893dd074e94ad66e9490be9f321803
slampana <justin.campana@gmail.com>
2013-10-18 14:45:24 -0700

    Initial commit

--- twitter-get-data.php
+++ twitter-get-data.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once('modules/config.php');
+include_once('modules/class.twitter.php');
 
 if(!loggedIn()):
     header('Location: login.php');
@@ -11,8 +12,9 @@ else:
     $twitter_oauth_token_secret = $query['twitter_oauth_token_secret'];
 
     if(isset($twitter_oauth_token) && isset($twitter_oauth_token_secret)):
-        echo "twitter_oauth_token --- " . $twitter_oauth_token;
-        echo "twitter_oauth_token_secret --- " . $twitter_oauth_token_secret;
+        $connection = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_oauth_token, $twitter_oauth_token_secret);
+        $content = $connection->get('users/show', array('screen_name' => 'RealRonHoward'));
+        var_dump($content);
     else:
         header('Location: twitter-login.php');
         exit();
