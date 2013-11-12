1cb2aa03959eb8feff0d964962a572e0a11af428
slampana <justin.campana@gmail.com>
2013-11-11 19:59:28 -0800

    Initial commit

--- facebook.php
+++ facebook.php
@@ -15,40 +15,18 @@ else:
 
     // get user access_token
     if (isset($code)):
-        $token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
-            . $facebook_app_id . '&redirect_uri=' . urlencode($facebook_auth_url)
-            . '&client_secret=' . $facebook_app_secret
-            . '&code=' . $code;
-        $access_token = substr(file_get_contents($token_url), 13);
-
-        // get extended user access_token
-        $extended_token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
-            . $facebook_app_id
-            . '&client_secret=' . $facebook_app_secret
-            . '&grant_type=fb_exchange_token'
-            . '&fb_exchange_token=' . $access_token;
-        $access_token = substr(file_get_contents($extended_token_url), 13);
-
-        getFacebookAccessToken($query['username'], $access_token);
+        $access_token = getFacebookAccessToken($code);
+        setFacebookAccessToken($query['username'], $access_token);
     endif;
 
     // run fql query
-    $fql_query_url = 'https://graph.facebook.com/fql?q='
-        . 'SELECT+name+FROM+user+WHERE+uid+IN+(SELECT+uid2+FROM+friend+WHERE+uid1+=+me())'
-        . '&access_token=' . $access_token;
-    $fql_query_result = curl_get_file_contents($fql_query_url);
-    $fql_query_obj = json_decode($fql_query_result);
-
-    $fql_query_url2 = 'https://graph.facebook.com/fql?q='
-        . 'SELECT+name,+page_url,+about+FROM+page+WHERE+page_id+IN+(SELECT+page_id+FROM+page_admin+WHERE+uid+=+me())'
-        . '&access_token=' . $access_token;
-    $fql_query_result2 = curl_get_file_contents($fql_query_url2);
-    $fql_query_obj2 = json_decode($fql_query_result2);
+    $fql_query_user = json_decode(getFacebookUser($access_token));
+    $fql_query_pages = json_decode(getFacebookPages($access_token));
 
     //Check for errors
-    if ($fql_query_obj->error):
+    if ($fql_query_user->error):
         // check to see if this is an oAuth error:
-        if ($fql_query_obj->error->type== "OAuthException"):
+        if ($fql_query_user->error->type== "OAuthException"):
             // Retrieving a valid access token.
             $dialog_url = "https://www.facebook.com/dialog/oauth?"
                 . "scope=" . $facebook_scope
@@ -62,13 +40,13 @@ else:
     else:
         // display results of fql query
         echo '<pre>';
-        print_r("Pages:");
-        print_r($fql_query_obj2);
+        print_r("User:");
+        print_r($fql_query_user);
         echo '</pre>';
         echo "<br><br>";
         echo '<pre>';
-        print_r("Friends:");
-        print_r($fql_query_obj);
+        print_r("Pages:");
+        print_r($fql_query_pages);
         echo '</pre>';
         echo "<br><br>";
     endif;
--- modules/func.facebook.php
+++ modules/func.facebook.php
@@ -1,6 +1,6 @@
 <?php
 
-function getFacebookAccessToken($username, $facebook_access_token)
+function setFacebookAccessToken($username, $facebook_access_token)
 {
     if (empty($facebook_access_token)):
         echo "No Facebook access token was given.";
@@ -8,9 +8,54 @@ function getFacebookAccessToken($username, $facebook_access_token)
         global $coll;
         $coll->update(array('username' => $username),
             array('$set' => array('facebook_access_token' => $facebook_access_token)
-            ));
+));
         return true;
     endif;
+}
+
+function getFacebookAccessToken($code)
+{
+    global $facebook_app_id;
+    global $facebook_auth_url;
+    global $facebook_app_secret;
+
+    $token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
+        . $facebook_app_id . '&redirect_uri=' . urlencode($facebook_auth_url)
+        . '&client_secret=' . $facebook_app_secret
+        . '&code=' . $code;
+    $access_token = substr(file_get_contents($token_url), 13);
+
+    // get extended user access_token
+    $extended_token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
+        . $facebook_app_id
+        . '&client_secret=' . $facebook_app_secret
+        . '&grant_type=fb_exchange_token'
+        . '&fb_exchange_token=' . $access_token;
+    return substr(file_get_contents($extended_token_url), 13);
+}
+
+function getFacebookUser($access_token)
+{
+    $fql_query_url = 'https://graph.facebook.com/fql?q='
+        . 'SELECT+first_name,+last_name,+username+FROM+user+WHERE+uid+=+me()'
+        . '&access_token=' . $access_token;
+    return curl_get_file_contents($fql_query_url);
+}
+
+function getFacebookPages($access_token)
+{
+    $fql_query_url = 'https://graph.facebook.com/fql?q='
+        . 'SELECT+name,+page_url,+about+FROM+page+WHERE+page_id+IN+(SELECT+page_id+FROM+page_admin+WHERE+uid+=+me())'
+        . '&access_token=' . $access_token;
+    return curl_get_file_contents($fql_query_url);
+}
+
+function getFacebookFriends($access_token)
+{
+    $fql_query_url = 'https://graph.facebook.com/fql?q='
+        . 'SELECT+name+FROM+user+WHERE+uid+IN+(SELECT+uid2+FROM+friend+WHERE+uid1+=+me())'
+        . '&access_token=' . $access_token;
+    return curl_get_file_contents($fql_query_url);
 }
 
 // note this wrapper function exists in order to circumvent PHP’s
