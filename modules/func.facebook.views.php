4c768058bdd410a0c44d25395fd7ea2f30e96d98
slampana <justin.campana@gmail.com>
2013-12-01 19:39:39 -0800

    Initial commit

--- modules/func.facebook.views.php
+++ modules/func.facebook.views.php
@@ -19,8 +19,8 @@ function getFacebookFans()
         $fql_query_page = json_decode(getFacebookPageDetails($access_token, $query['facebook_page'][0]['facebook_page_id']), true);
 
         //Check for errors
-        if ($fql_query_page['error']):
-            if ($fql_query_page['error']['type'] == "OAuthException"):
+        if(isset($fql_query_page['error'])):
+            if($fql_query_page['error']['type'] == "OAuthException"):
                 callFacebookAuth();
             else:
                 return "Other Facebook authentication error has happened";
