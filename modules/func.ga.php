81616a961784bfbf689b888a960fbe1e5d930794
slampana <justin.campana@gmail.com>
2013-12-10 20:39:28 -0800

    Initial commit

--- modules/func.ga.php
+++ modules/func.ga.php
@@ -118,9 +118,9 @@ function getAccessToken($refresh_token)
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
         $data = curl_exec($ch);
+        echo 'Result 2: ' . $ch;
         curl_close($ch);
         $result = json_decode($data, true);
-        echo 'Result: ' . $ch;
         return $result['access_token'];
     endif;
 }
